0b08f301b4c9fe6ea45186fc7bd72c3df442b7a0
bayan kassem <bayanka@post.bgu.ac.il>
2024-09-03 12:04:37 +0300

    added backend base

---
+++ src/main/java/Backend/Controller.java
@@ -0,0 +1,13 @@
+package Backend;
+
+public class Controller {
+    private String current_id; //fetch the ID from the database
+
+
+    public Controller()
+    {
+        //fetch id from DB
+    }
+
+
+}
