41d148c95dbcc8334de6a316996dd99ecc13763f
Amihai Blekherov <amihaibl@post.bgu.ac.il>
2024-09-05 11:50:45 +0300

    Fixed the issue with the table, working as it should now

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,11 @@ module Frontend {
     requires javafx.fxml;
     requires java.sql;
 
-
+    opens Backend to javafx.fxml, javafx.base;
+    opens Backend.Business_Layer to javafx.fxml, javafx.base;
+    opens Backend.Service_Layer to javafx.fxml, javafx.base;
     opens Frontend to javafx.fxml;
     exports Frontend;
+    exports Backend.Business_Layer;
+    exports Backend.Service_Layer;
 }
